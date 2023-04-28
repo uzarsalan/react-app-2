daa5bc8822db08216cb3d4328a1d8a75f4d9459b
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-04-28 23:03:00 +0300

    visual fixes

--- src/components/Menu/MenuCard/index.js
+++ src/components/Menu/MenuCard/index.js
@@ -14,7 +14,7 @@ const MenuCard = ({ item, onClickBuy = () => {} }) => {
       <img src={item.image} className="w-full" />
       <div className="p-2">
         <div className={"text-2xl font-bold " + style.title}>{item.name}</div>
-        <div className="p-2">{ingredients}</div>
+        <div className="py-2">{ingredients}</div>
         <button
           onClick={() => onClickBuy(item)}
           className="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -34,7 +34,7 @@ const Menu = ({ onItemBuy }) => {
     },
   ];
   return (
-    <div className={"grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4"}>
+    <div className={"grid gap-4 grid-cols-2 md:grid-cols-3 xl:grid-cols-4"}>
       {menu.map((item) => (
         <MenuCard item={item} onClickBuy={onItemBuy} />
       ))}
