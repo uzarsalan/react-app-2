8a19ec120a44fbba578e1b8f5828bea253885d1b
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-04-28 21:12:49 +0300

    class -> className fix

--- src/components/Menu/MenuCard/index.js
+++ src/components/Menu/MenuCard/index.js
@@ -6,14 +6,14 @@ const MenuCard = ({ item, onClickBuy = () => {} }) => {
   );
 
   return (
-    <div class={"rounded overflow-hidden shadow-lg " + style.card}>
+    <div className={"rounded overflow-hidden shadow-lg " + style.card}>
       <img src={item.image} className="w-full" />
       <div className="p-2">
         <div className={"text-2xl font-bold " + style.title}>{item.name}</div>
         <div className="p-2">{ingredients}</div>
         <button
           onClick={() => onClickBuy(item)}
-          class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          className="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
         >
           Купить
         </button>
