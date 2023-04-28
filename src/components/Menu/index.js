b16a867220c1a77661b424d8e415959ce9188871
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-04-28 22:55:35 +0300

    img path fix 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,31 +6,31 @@ const Menu = ({ onItemBuy }) => {
       name: "Куриная",
       price: 500,
       ingredients: ["Грибы", "Курица", "Сыр", "Томатный соус"],
-      image: "react-app-2/assets/pizza.jpeg",
+      image: "/react-app-2/assets/pizza.jpeg",
     },
     {
       name: "Колбасная",
       price: 500,
       ingredients: ["Грибы", "Колбаса", "Сыр", "Томатный соус"],
-      image: "react-app-2/assets/pizza.jpeg",
+      image: "/react-app-2/assets/pizza.jpeg",
     },
     {
       name: "Ананасная",
       price: 500,
       ingredients: ["Грибы", "Ананас", "Сыр", "Томатный соус"],
-      image: "react-app-2/assets/pizza.jpeg",
+      image: "/react-app-2/assets/pizza.jpeg",
     },
     {
       name: "Охотничья",
       price: 500,
       ingredients: ["Грибы", "Колбаски", "Сыр", "Томатный соус"],
-      image: "react-app-2/assets/pizza.jpeg",
+      image: "/react-app-2/assets/pizza.jpeg",
     },
     {
       name: "Детская",
       price: 500,
       ingredients: ["Грибы", "Картофель фри", "Сыр", "Томатный соус"],
-      image: "react-app-2/assets/pizza.jpeg",
+      image: "/react-app-2/assets/pizza.jpeg",
     },
   ];
   return (
